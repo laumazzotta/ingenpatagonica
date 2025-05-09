1bdf557cdc7d4aa5ce1fb87838450ae225a7cbdf
laumazzotta <lau.mazzotta@gmail.com>
2025-05-09 09:35:52 -0300

    ajustes animaciones

--- scripts.js
+++ scripts.js
@@ -9,18 +9,6 @@ jQuery(function($) {
 
 });
 
-// let navButton = document.getElementById('nav-icon3');
-// let navMobile = document.getElementById('myNav');
-// let brand = document.getElementById('brand');
-// let navLinks = document.getElementById('navLinks');
-
-// navButton.onclick = function() {
-//     this.classList.toggle('open');
-//     navMobile.classList.toggle('h-100');
-//     brand.classList.toggle('width-100px');
-
-// }
-
 $('#openNav').on('click', function() {
     $('#myNav').css("height", "100%");
 });
@@ -32,20 +20,6 @@ $('#closeNav').on('click', function() {
 $('.nav-link').on('click', function() {
     $('#myNav').css("height", "0%");
 });
-
-
-// gsap.registerPlugin(ScrollTrigger) 
-
-// gsap.to('.imagen-mision-vision', {
-//     scrollTrigger: {
-//         trigger: '.imagen-mision-vision',
-//         // start:  "top ",
-//         scrub: true
-//     },
-//     x: "0",
-//     duration: 1,
-//     // ease: "power3.in"
-// });
 
 var $plusButton = $('.plus-button'); 
 
